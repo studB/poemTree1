e4c45e3b2936b0720377fdeb7bf97c6b1e491ee9
DaeYoungBae <bdy6290@naver.com>
2021-01-08 22:58:54 +0900

    Init push

---
+++ src/main/java/practice/poemtree1/Poemtree1Application.java
@@ -0,0 +1,13 @@
+package practice.poemtree1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poemtree1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poemtree1Application.class, args);
+	}
+
+}
---
+++ src/test/java/practice/poemtree1/Poemtree1ApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.poemtree1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poemtree1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
